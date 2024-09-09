b28c8b1c689f9290ae96067445b7d8a30d0c3c2e
Olivier Bergeron-Houde <1732451@etu.cchic.ca>
2024-09-09 08:52:57 -0400

    map + characters

--- API/RPG_API/Models/Map.cs
+++ API/RPG_API/Models/Map.cs
@@ -2,5 +2,11 @@
 {
     public class Map
     {
+        public int Id { get; set; }
+        public List<List<int>> Coordinates { get; set; }
+        public string ImageUrl { get; set; }
+
+
+        
     }
 }
