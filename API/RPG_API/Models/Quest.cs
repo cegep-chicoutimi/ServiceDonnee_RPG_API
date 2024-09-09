b576050edc3e98a3fa5604ba89d49473ed06cac3
Samara Boudreault <2230912@etu.cchic.ca>
2024-09-09 08:49:25 -0400

    Nuget mis et 3/5 class faite

--- API/RPG_API/Models/Quest.cs
+++ API/RPG_API/Models/Quest.cs
@@ -2,5 +2,10 @@
 {
     public class Quest
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Reward { get; set; }
+        public bool Status { get; set; }
     }
 }
--- API/RPG_API/Models/Tile.cs
+++ API/RPG_API/Models/Tile.cs
@@ -1,6 +1,16 @@
 ﻿namespace RPG_API.Models
 {
+    public enum Type
+    {
+        Easy,
+        Medium,
+        Hard, 
+        Boss
+    }
     public class Tile
     {
+        public Type Type { get; set; }
+        public int Y {get; set; }
+        public int X { get; set; }
     }
 }
