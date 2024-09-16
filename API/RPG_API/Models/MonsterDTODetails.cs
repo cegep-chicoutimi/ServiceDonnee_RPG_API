984e5aadfa6270d06f447efbba2caa40d724f9fc
Olivier Bergeron-Houde <1732451@etu.cchic.ca>
2024-09-16 08:47:06 -0400

    DTO

---
+++ API/RPG_API/Models/MonsterDTODetails.cs
@@ -0,0 +1,15 @@
+﻿namespace RPG_API.Models
+{
+    public class MonsterDTODetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TypeMonster Type { get; set; }
+        public Map Map { get; set; }
+
+        public static MonsterDTODetails MonsterToDTO(Monster m)
+        {
+            return new MonsterDTODetails { Id = m.Id, Name = m.Name, Type = m.Type };
+        }
+    }
+}
